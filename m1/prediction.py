74a075928f6f1f6a4c28e9cee29798faca8b25fc
qmShen <joyshen06@gmail.com>
2017-05-07 23:24:00 +0800

    save

--- m1/prediction.py
+++ m1/prediction.py
@@ -45,7 +45,7 @@ transformer.set_transpose('data', (2,0,1))
 Making predicitions
 '''
 ##Reading image paths
-test_img_paths = [img_path for img_path in glob.glob("../data/test/*jpg")]
+test_img_paths = [img_path for img_path in glob.glob("data/test/*jpg")]
 
 test_ids = []
 preds = []
